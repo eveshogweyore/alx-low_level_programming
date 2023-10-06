d486e1dbd9444fc2d09dfd0c6840d54addc8c355
Eveshogweyore <alleeveshogweyore@gmail.com>
2023-10-06 10:27:47 -0700

    Task 10 REDONE

--- 0x01-variables_if_else_while/100-print_comb3.c
+++ 0x01-variables_if_else_while/100-print_comb3.c
@@ -9,18 +9,21 @@ int main(void)
 {
 	int i = 0;
 	int j = 1;
+	int track = 0;
 
 	for (; i < 9; i++)
 	{
 		/*printf("i is %d\n", i);*/
 		for (; j <= 9; j++)
 		{
-			if (i == j)
+			if (i == j || track == j)
 				continue;
 			putchar(i + '0');
 			putchar(j + '0');
+			putchar(',');
 			putchar(' ');
 		}
+		track = i;
 		j = 0;
 	}
 
