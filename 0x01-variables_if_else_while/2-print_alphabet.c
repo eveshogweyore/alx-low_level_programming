4efe757316b895cf38e35a1c1cbf0735cd1905f3
Eveshogweyore <alleeveshogweyore@gmail.com>
2023-10-06 06:11:34 -0700

    Task 2 DONE

---
+++ 0x01-variables_if_else_while/2-print_alphabet.c
@@ -0,0 +1,21 @@
+#include <stdio.h>
+
+/**
+ * main - Entry point
+ * Purpose: Output all lower case alphabets
+ * Return: Always 0 (Success)
+ */
+int main(void)
+{
+	int ascii_alphabet_start = 97;
+	int ascii_alphabet_end = 122;
+
+	while (ascii_alphabet_start <= ascii_alphabet_end)
+	{
+		putchar((char)ascii_alphabet_start);
+		putchar('\n');
+		ascii_alphabet_start++;
+	}
+
+	return (0);
+}
