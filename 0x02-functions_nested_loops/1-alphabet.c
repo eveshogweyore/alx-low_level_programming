ab736af61b4eefae8617f4bb13ae8b3fe7cc29b8
Eveshogweyore <alleeveshogweyore@gmail.com>
2023-10-10 07:41:08 -0700

    Task 1 DONE

--- 0x02-functions_nested_loops/1-alphabet.c
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -1,17 +1,6 @@
 #include "main.h"
 
 /**
- * main - check the code
- *
- * Return: Always 0.
- */
-int main(void)
-{
-	print_alphabet();
-	return (0);
-}
-
-/**
   * print_alphabet - prints alphabets a - z
   *
   * Return: Always void
