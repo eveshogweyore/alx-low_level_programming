481304c5f9f23ecf754170542cce977aadaa15df
Eveshogweyore <alleeveshogweyore@gmail.com>
2023-10-10 07:37:16 -0700

    Task 1 DONE

--- 0x02-functions_nested_loops/1-alphabet.c
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -11,9 +11,15 @@ int main(void)
 	return (0);
 }
 
-void print_alphabet()
+/**
+  * print_alphabet - prints alphabets a - z
+  *
+  * Return: Always void
+  */
+void print_alphabet(void)
 {
 	char alphabet = 'a';
+
 	for (; alphabet <= 'z'; alphabet++)
 	{
 		_putchar(alphabet);
