39d0ada4eddf5430ee22a2733370e9808af9201b
Eveshogweyore <alleeveshogweyore@gmail.com>
2023-10-10 19:24:29 -0700

    Task 11 DONE

---
+++ 0x02-functions_nested_loops/11-print_to_98.c
@@ -0,0 +1,64 @@
+#include "main.h"
+
+/**
+ * print_to_98 - Print numbers from n to 98
+ * @n: The first number
+ *
+ * Return: void
+ */
+void print_to_98(int n)
+{
+	if (n <= 98 ) 
+	{
+		for (; n <= 98; n++)
+		{
+			if (n < 0) 
+			{
+				if (n >= -9 )
+				{
+					_putchar('-');
+					_putchar((n * -1) + 48);
+				} else
+				{
+					_putchar('-');
+					_putchar(((n * -1) / 10) + 48);
+					_putchar(((n * -1) % 10) + 48);
+				}
+			}
+			if (n >= 0 && n <= 9)
+				_putchar(n + 48);
+			if (n > 9)
+			{
+				_putchar((n / 10) + 48);
+				_putchar((n % 10) + 48);
+			}
+			if (n != 98)
+			{
+				_putchar(',');
+				_putchar(' ');
+			}
+		}
+	} else
+	{
+		for (; n >= 98; n--)
+		{
+			if (n > 99) 
+			{
+				_putchar(((n / 10) / 10) + 48);
+				_putchar(((n / 10) % 10) + 48);
+				_putchar((n % 10) + 48);
+
+			} else 
+			{
+				_putchar((n / 10) + 48);
+				_putchar((n % 10) + 48);
+			}
+			if (n != 98)
+			{
+				_putchar(',');
+				_putchar(' ');
+			}
+		}
+	}
+	_putchar('\n');
+}
