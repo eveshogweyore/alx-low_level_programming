a08b36d31f66d31425604e7f2aa1b4b265b66081
Eveshogweyore <alleeveshogweyore@gmail.com>
2023-10-11 17:02:08 -0700

    Task 11 MODIFIED

--- 0x02-functions_nested_loops/11-print_to_98.c
+++ 0x02-functions_nested_loops/11-print_to_98.c
@@ -1,67 +1,27 @@
+#include <stdio.h>
 #include "main.h"
 
 /**
  * print_to_98 - Print numbers from n to 98
- * @n: The first number
+ * @num: The first number
  *
  * Return: void
  */
-void print_to_98(int n)
+void print_to_98(int num)
 {
-	if (n <= 98)
+	if (num > 98)
 	{
-		for (; n <= 98; n++)
+		for (; num > 98; num--)
 		{
-			if (n < 0)
-			{
-				_putchar('-');
-				if (n >= -9)
-				{
-					_putchar((n * -1) + 48);
-				} else if (n >= -99)
-				{
-					_putchar(((n * -1) / 10) + 48);
-					_putchar(((n * -1) % 10) + 48);
-				} else
-				{
-					_putchar((((n * -1) / 10) / 10) + 48);
-					_putchar((((n * -1) / 10) % 10) + 48);
-					_putchar(((n * -1) % 10) + 48);
-				}
-			}
-			if (n >= 0 && n <= 9)
-				_putchar(n + 48);
-			if (n > 9)
-			{
-				_putchar((n / 10) + 48);
-				_putchar((n % 10) + 48);
-			}
-			if (n != 98)
-			{
-				_putchar(',');
-				_putchar(' ');
-			}
-		}
-	} else
-	{
-		for (; n >= 98; n--)
-		{
-			if (n > 99)
-			{
-				_putchar(((n / 10) / 10) + 48);
-				_putchar(((n / 10) % 10) + 48);
-
-			} else
-			{
-				_putchar((n / 10) + 48);
-			}
-			_putchar((n % 10) + 48);
-			if (n != 98)
-			{
-				_putchar(',');
-				_putchar(' ');
-			}
+			printf("%d, ", num);
 		}
 	}
-	_putchar('\n');
+	else if (num < 98)
+	{
+		for (; num < 98; num++)
+		{
+			printf("%d, ", num);
+		}
+	}
+	printf("%d\n", num);
 }
