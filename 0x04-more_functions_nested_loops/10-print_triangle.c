e2920f903df3f01eb543862188dcc38ad3f88842
Eveshogweyore <alleeveshogweyore@gmail.com>
2023-10-12 12:44:34 -0700

    Task 10 DONE

---
+++ 0x04-more_functions_nested_loops/10-print_triangle.c
@@ -0,0 +1,24 @@
+#include "main.h"
+
+/**
+ * print_triangle - Print straight line in terminal
+ * @n: specifies the height of triangle
+ *
+ * Return: void
+ */
+void print_triangle(int size)
+{
+	int i, j, k;
+
+	if (size <= 0)
+		_putchar('\n');
+
+	for (i = 1; i <= size; i++)
+	{
+		for (j = 0; j < size - i; j++)
+			_putchar(' ');
+		for (k = 0; k < size - j; k++)
+			_putchar('#');
+		_putchar('\n');
+	}
+}
