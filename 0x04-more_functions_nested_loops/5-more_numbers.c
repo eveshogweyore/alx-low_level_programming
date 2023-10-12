86440cf9bb1b2a597a1ef446e0ee9b32de79b6ea
Eveshogweyore <alleeveshogweyore@gmail.com>
2023-10-12 09:51:26 -0700

    Task 5 DONE

--- 0x04-more_functions_nested_loops/5-more_numbers.c
+++ 0x04-more_functions_nested_loops/5-more_numbers.c
@@ -8,7 +8,7 @@
 void more_numbers(void)
 {
 	int i, j;
-	
+
 	for (i = 0; i < 10; i++)
 	{
 		for (j = 0; j <= 14; j++)
@@ -17,6 +17,6 @@ void more_numbers(void)
 				_putchar((j / 10) + '0');
 			_putchar((j % 10) + '0');
 		}
+		_putchar('\n');
 	}
-	_putchar('\n');
 }
