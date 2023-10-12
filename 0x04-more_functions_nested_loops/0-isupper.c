6c31cef47344e57e9ba1cb830ace8a05d98aa8ce
Eveshogweyore <alleeveshogweyore@gmail.com>
2023-10-12 06:28:59 -0700

    Task 0 DONE

---
+++ 0x04-more_functions_nested_loops/0-isupper.c
@@ -0,0 +1,17 @@
+#include "main.h"
+
+/**
+ * _isupper - check if character is upper case
+ * @c: The character to check for
+ *
+ * Return: On success 1.
+ * On error, 0 is returned.
+ */
+int _islower(int c)
+{
+	c = (char)c;
+
+	if (c >= 'A' && c <= 'B')
+		return (1);
+	return (0);
+}
