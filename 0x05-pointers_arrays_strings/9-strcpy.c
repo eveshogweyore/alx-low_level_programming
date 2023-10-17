dfd30f7e48e41694b288b19bdf3b22ee74c97392
Eveshogweyore <alleeveshogweyore@gmail.com>
2023-10-17 02:31:29 -0700

    Task 9 DONE

---
+++ 0x05-pointers_arrays_strings/9-strcpy.c
@@ -0,0 +1,21 @@
+#include "main.h"
+
+/**
+ * *_strcpy - copies the string pointed to by a 
+ * var to another variable
+ * @dest: The destination variable
+ * @src: The source variable
+ *
+ * Return: The pointer to dest
+ */
+char *_strcpy(char *dest, char *src)
+{
+	int i = 0;
+	
+	while (src)
+	{
+		dest[i] = src[i];
+		i++;
+	}
+	return (&dest[0]);
+}
