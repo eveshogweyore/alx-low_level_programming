802689af428b822a6d6a9c2ce930ad0085d25a18
Eveshogweyore <alleeveshogweyore@gmail.com>
2023-10-17 00:28:54 -0700

    Task 7 DONE

--- 0x05-pointers_arrays_strings/7-puts_half.c
+++ 0x05-pointers_arrays_strings/7-puts_half.c
@@ -16,7 +16,7 @@ void puts_half(char *str)
 		length++;
 		end++;
 	}
-	i % 2 != 0 ? i = (length / 2) + 1 : i = (length / 2);
+	i = (i % 2 != 0) ? (length / 2) + 1 : length / 2;
 	for (; i < length; i++)
 		_putchar(str[i]);
 	_putchar('\n');
