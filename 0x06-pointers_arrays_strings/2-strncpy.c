b42ea340031b45945e99900adffc8348f78b8ff6
Eveshogweyore <alleeveshogweyore@gmail.com>
2023-10-18 06:48:04 -0700

    Task 2 DONE

---
+++ 0x06-pointers_arrays_strings/2-strncpy.c
@@ -0,0 +1,22 @@
+#include "main.h"
+
+/**
+ * *_strncpy - copies a string from one var to another
+ * @dest: The string to be copied into
+ * @src: The string to copy
+ * @n: The number of characters in the src var to be copied
+ *
+ * Return: Pointer to the start of the copied string
+ */
+char *_strncat(char *dest, char *src, int n)
+{
+	int i = 0;
+	char *end = dest;
+
+	while (i < n)
+	{
+		*(end + i) = *(src + i);
+		i++;
+	}
+	return (dest);
+}
