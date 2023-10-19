6591b69b4652a4095e2297ba5f97d12535710c58
Eveshogweyore <alleeveshogweyore@gmail.com>
2023-10-19 08:14:56 -0700

    Task 2 DONE

--- 0x06-pointers_arrays_strings/2-strncpy.c
+++ 0x06-pointers_arrays_strings/2-strncpy.c
@@ -12,12 +12,12 @@ char *_strncpy(char *dest, char *src, int n)
 {
 	int i = 0;
 
-	while (i < n)
+	while (i < n && src[i] != '\0')
 	{
 		dest[i] = src[i];
-		if (src[i] == '\0')
-			break;
 		i++;
 	}
+	for (; i < n; i++)
+		dest[i] = '\0';
 	return (dest);
 }
