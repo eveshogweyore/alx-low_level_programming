1b2930eba9983e74dcc7c545bc9c2346672707a3
Eveshogweyore <alleeveshogweyore@gmail.com>
2023-10-18 23:00:16 -0700

    Task 5 DONE

---
+++ 0x06-pointers_arrays_strings/5-string_toupper.c
@@ -0,0 +1,19 @@
+#include "main.h"
+
+/**
+ * *string_toupper - changes lower case letters to uppercase
+ * @str: The string to be manipulated
+ *
+ * Return: void
+ */
+char *string_toupper(char *str)
+{
+	int i = 0;
+
+	while (*(str + i))
+	{
+		if (*(str + i) >= 'a' && *(str + i) <= 'z')
+			*(str + i) -= 32;
+		i++;
+	}
+}
