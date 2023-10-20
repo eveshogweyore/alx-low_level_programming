8c32a091645578d366bb90a94c1a540e1415a114
Eveshogweyore <alleeveshogweyore@gmail.com>
2023-10-20 08:18:11 -0700

    Task 8 DONE

--- 0x06-pointers_arrays_strings/100-rot13.c
+++ 0x06-pointers_arrays_strings/100-rot13.c
@@ -8,14 +8,15 @@
  */
 char *rot13(char *str)
 {
-	i = 0;
+	int i = 0;
 
 	while (str[i] != '\0')
 	{
-		if (str[i] >= 'a' && str[i] <= 'm' || str[i] >= 'A' && str[i] <= 'M')
-			str[i] += 13;
-		if (str[i] >= 'n' && str[i] <= 'z' || str[i] >= 'N' && str[i] <= 'Z')
+		if ((str[i] >= 'n' && str[i] <= 'z') || (str[i] >= 'N' && str[i] <= 'Z'))
 			str[i] -= 13;
+		else if ((str[i] >= 'a' && str[i] <= 'm') || (str[i] >= 'A' && str[i] <= 'M'))
+			str[i] += 13;
+		i++;
 	}
 	return (str);
 }
