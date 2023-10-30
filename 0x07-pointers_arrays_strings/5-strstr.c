69638590192bd5332d08ef5b614e7c77db7bcfcd
Eveshogweyore <alleeveshogweyore@gmail.com>
2023-10-30 06:18:25 -0700

    Files rearranged

--- 0x07-pointers_arrays_strings/5-strstr.c
+++ 0x07-pointers_arrays_strings/5-strstr.c
@@ -1,5 +1,5 @@
 #include "main.h"
-
+#include <stdio.h>
 /**
  * _strstr - locates a substring
  * @haystack: The main string
@@ -11,24 +11,26 @@
 char *_strstr(char *haystack, char *needle)
 {
 	unsigned int i, j, movei, match = 0;
+	char *p = needle;
 
 	for (i = 0; haystack[i] != '\0'; i++)
 	{
-		if (needle[0] == haystack[i])
+		printf("i is %d", i);
+		if (*p == haystack[i])
 		{
 			movei = i + 1;
-			for (j = 1; needle[j] != '\0'; j++, movei++)
+			for (j = 1; p[j] != '\0'; j++, movei++)
 			{
 				if (haystack[movei] == '\0')
 					break;
-				if (needle[j] == haystack[movei])
+				if (p[j] == haystack[movei])
 					match = 1;
 				else
 					match = 0;
 			}
 		}
 		if (match == 1)
-			break;
+			return ("Working...");
 	}
-	return (haystack + i);
+	return (0);
 }
---
+++ 0x0B-malloc_free/0-create_array.c
@@ -0,0 +1,7 @@
+#include "main.h"
+
+char *create_array(unsigned int size, char c)
+{
+
+	return;
+}
