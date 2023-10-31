6a722e951a4334a570b53dfac862f62d848ab773
Eveshogweyore <alleeveshogweyore@gmail.com>
2023-10-31 08:04:47 -0700

    Task 4 DONE

--- 0x0B-malloc_free/4-free_grid.c
+++ 0x0B-malloc_free/4-free_grid.c
@@ -4,7 +4,7 @@
  * free_grid - frees a 2-Dimensional grid
  * @grid: the 2D grid to be freed
  * @height: the height of the grid
- * 
+ *
  * Return: nothing
  */
 void free_grid(int **grid, int height)
