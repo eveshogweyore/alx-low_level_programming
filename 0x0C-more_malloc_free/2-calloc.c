0b90086a0f8bdfdf415d13e678bcccc9e00724f8
Eveshogweyore <alleeveshogweyore@gmail.com>
2023-11-02 07:19:47 -0700

    Task 2 DONE

--- 0x0C-more_malloc_free/2-calloc.c
+++ 0x0C-more_malloc_free/2-calloc.c
@@ -16,7 +16,7 @@ void *_calloc(unsigned int nmemb, unsigned int size)
 	if (nmemb == 0 || size == 0)
 		return (NULL);
 
-	arraymem = malloc(nmemb * sizeof(size));
+	arraymem = malloc(nmemb * size);
 	if (arraymem == NULL)
 		return (NULL);
 
