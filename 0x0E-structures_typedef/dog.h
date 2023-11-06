c986d60a5eb156fa92d6a47593c47c0de36a687d
Eveshogweyore <alleeveshogweyore@gmail.com>
2023-11-06 14:31:43 -0800

    Task 0 DONE

---
+++ 0x0E-structures_typedef/dog.h
@@ -0,0 +1,19 @@
+#ifndef _DOG_H_
+#define _DOG_H_
+
+/**
+ * struct dog - defines a dog
+ * @name: name of dog
+ * @age: age of dog
+ * @owner: owner of dog
+ *
+ * Description: defines a dog with properties name, age and owner
+ */
+struct dog
+{
+	char *name;
+	float age;
+	char *owner;
+};
+
+#endif
