e68dff1d883c144b729064c00be4ba8081963b7a
Eveshogweyore <alleeveshogweyore@gmail.com>
2023-11-08 03:44:09 -0800

    Task 0 DONE

---
+++ 0x0F-function_pointers/0-print_name.c
@@ -0,0 +1,13 @@
+#include "function_pointers.h"
+
+/**
+ * print_name - function that prints a name
+ * @name: name to be printed
+ * @f: pointer to function that prints a name
+ *
+ * Return: nothing
+ */
+void print_name(char *name, void (*f)(char *))
+{
+	(*f)(name);
+}
