8e20ca8064f6c4659dc5517d18ce286cc0291db2
Eveshogweyore <alleeveshogweyore@gmail.com>
2023-11-16 05:16:53 -0800

    Task 0 DONE

--- 0x12-singly_linked_lists/0-print_list.c
+++ 0x12-singly_linked_lists/0-print_list.c
@@ -13,7 +13,7 @@ size_t print_list(const list_t *h)
 
 	while (h != NULL)
 	{
-		if (!(h->str))
+		if (h->str == NULL)
 			printf("[%lu] (nil)\n", i);
 		else
 			printf("[%u] %s\n", h->len, h->str);
