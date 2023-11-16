d8f8273e8d2c289588ac46577e0a3b9866e8314a
Eveshogweyore <alleeveshogweyore@gmail.com>
2023-11-16 05:03:51 -0800

    Task 0 DONE

---
+++ 0x12-singly_linked_lists/0-print_list.c
@@ -0,0 +1,44 @@
+#include <stdio.h>
+#include "lists.h"
+
+/**
+ * _strlen - returns length of string
+ * @str: string to check for
+ *
+ * Return: length of string
+ */
+size_t _strlen(char *str)
+{
+	size_t length = 0;
+
+	while (str[length])
+		length++;
+
+	return (length);
+}
+
+/**
+ * prints - prints all elements of a list_t list
+ * @h: each node in the list
+ *
+ * Return: size of list
+ */
+size_t print_list(const list_t *h)
+{
+	size_t i = 0;
+
+	if (!h)
+		return (0);
+
+	while (h)
+	{
+		if (!(h->str))
+			printf("[%lu] (nil)\n", i);
+		else
+			printf("[%lu] %s\n", _strlen(h->str), h->str);
+		h = h->next;
+		i++;
+	}
+
+	return (i);
+}
