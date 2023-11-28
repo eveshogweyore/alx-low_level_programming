3e4d31101f5f5c5ac4ac86ba6110cc3a76910a5a
Eveshogweyore <alleeveshogweyore@gmail.com>
2023-11-28 13:27:07 -0800

    Task 1 DONE

--- 0x15-file_io/1-create_file.c
+++ 0x15-file_io/1-create_file.c
@@ -11,18 +11,21 @@
  */
 int create_file(const char *filename, char *text_content)
 {
-	int file_d, length;
+	int file_d, i, w_length;
 	mode_t mode = S_IRUSR | S_IWUSR;
+
+	for (i = 0; text_content[i]; i++)
+		;
 
 	if (!filename)
 		return (-1);
 
-	file_d = open(filename, O_CREAT | O_TRUNC, mode);
+	file_d = open(filename, O_CREAT | O_RDWR | O_TRUNC, mode);
 	if (file_d == -1)
 		return (-1);
 
-	length = write(file_d, text_content, sizeof(text_content));
-	if (length == -1)
+	w_length = write(file_d, text_content, i);
+	if (w_length == -1)
 		return (-1);
 
 	return (1);
