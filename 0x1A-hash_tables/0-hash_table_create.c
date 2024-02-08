5217a8bebf9a8cf3e512926c6c3b509569f713bb
Eveshogweyore <alleeveshogweyore@gmail.com>
2024-02-08 01:28:35 -0800

    Conform code to Betty standard.

--- 0x1A-hash_tables/0-hash_table_create.c
+++ 0x1A-hash_tables/0-hash_table_create.c
@@ -17,7 +17,7 @@ hash_table_t *hash_table_create(unsigned long int size)
 		return (NULL);
 
 	ht->size = size;
-	ht->array = malloc(sizeof(hash_node_t*) * ht->size);
+	ht->array = malloc(sizeof(hash_node_t *) * ht->size);
 
 	return (ht);
 }
