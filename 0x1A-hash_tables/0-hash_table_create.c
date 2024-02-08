e74e68cfd02cb44882fab5e4551da9cd249ce90b
Eveshogweyore <alleeveshogweyore@gmail.com>
2024-02-08 01:26:43 -0800

    Modify memory allocation.

--- 0x1A-hash_tables/0-hash_table_create.c
+++ 0x1A-hash_tables/0-hash_table_create.c
@@ -11,15 +11,13 @@ hash_table_t *hash_table_create(unsigned long int size)
 {
 	hash_table_t *ht;
 
-	if (size < 1)
-		return (NULL);
-
 	/* Create new hash table */
 	ht = malloc(sizeof(hash_table_t));
 	if (ht == NULL)
 		return (NULL);
 
 	ht->size = size;
+	ht->array = malloc(sizeof(hash_node_t*) * ht->size);
 
 	return (ht);
 }
