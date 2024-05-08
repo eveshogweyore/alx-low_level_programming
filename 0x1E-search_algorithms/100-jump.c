68a66fc0d411f779c56f19d79ce3bd0cc51c5f66
Eveshogweyore Alle <alleeveshogweyore@gmail.com>
2024-05-08 14:08:37 +0100

    Refactor codebase to use jump start algorithm pseudocode found on wikimedia.

--- 0x1E-search_algorithms/100-jump.c
+++ 0x1E-search_algorithms/100-jump.c
@@ -2,6 +2,21 @@
 #include <math.h>
 
 /**
+ * _min - checks for the minimum of two integer numbers.
+ * @a: The first number.
+ * @b: The second number.
+ *
+ * Return: On success, the minimum number.
+ */
+size_t _min(size_t a, size_t b)
+{
+	if (a < b)
+		return (a);
+
+	return (b);
+}
+
+/**
  * jump_search - jump search for a value in a sorted array.
  * @array: pointer to the first element of the array to search in.
  * @size: number of elements in the array.
@@ -12,32 +27,40 @@
  */
 int jump_search(int *array, size_t size, int value)
 {
-	size_t jump = sqrt(size);
-	size_t low = 0, high = jump;
+	size_t a = 0, j = sqrt(size), b = j;
 
 	if (!array)
 		return (-1);
 
-	for (; low < size; low += jump, high += jump)
+	while (array[b] < value)
 	{
-		printf("Value checked array[%ld] = [%d]\n", low, array[low]);
-
-		if (value <= array[high] || high >= size)
+		printf("Value checked array[%lu] = [%d]\n", a, array[a]);
+		a = b;
+		b += j;
+		if (a >= size)
 		{
-			printf("Value found between indexes [%lu] and [%lu]\n", low, high);
+			a -= j;
+			b -= j;
 			break;
 		}
 	}
 
-	if (high >= size)
-		high = size - 1;
+	if (b < size)
+		printf("Value checked array[%lu] = [%d]\n", a, array[a]);
+	printf("Value found between indexes [%lu] and [%lu]\n", a, b);
 
-	for (; low <= high; low++)
+	while (array[a] < value)
 	{
-		printf("Value checked array[%ld] = [%d]\n", low, array[low]);
+		printf("Value checked array[%lu] = [%d]\n", a, array[a]);
+		a += 1;
+		if (a == _min(b, size))
+			break;
+	}
 
-		if (array[low] == value)
-			return (low);
+	if (array[a] == value)
+	{
+		printf("Value checked array[%lu] = [%d]\n", a, array[a]);
+		return (a);
 	}
 
 	return (-1);
